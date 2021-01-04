10ed3c178f1adf490414c8d02a18e41cfd7e3493
minericefield <minericefield@gmail.com>
2021-01-04 21:05:09 +0900

    introducing TheModalItems to TheModal

--- src/components/TheModal/Component.vue
+++ src/components/TheModal/Component.vue
@@ -2,6 +2,11 @@
   <transition name="the-modal-fade" appear>
     <div class="the-modal d-flex align-items-center justify-content-center fixed-top w-100 h-100 p-5">
       <div class="the-modal-content d-flex flex-column w-100">
+        <the-modal-items
+          :items="editingItems"
+          @on-check="checkItem"
+          class="flex-grow-1 overflow-auto"
+        />
         <nav class="navbar navbar-light bg-light">
           <button
             @click="$emit('on-cancel-click')"
@@ -27,7 +32,9 @@ import { PropType } from 'vue'
 import { Item, ItemCheckable } from '@/types'
 
 import { Options, Vue, prop } from 'vue-class-component'
-import { getInitializedEditingItems } from './module'
+import { getCheckedItems, getInitializedEditingItems } from './module'
+
+import TheModalItems from '@/components/TheModal/Items/Component.vue'
 
 class Props {
   public items = prop({
@@ -36,6 +43,9 @@ class Props {
   })
 }
 @Options({
+  components: {
+    TheModalItems
+  },
   emits: ['on-cancel-click']
 })
 export default class TheModal extends Vue.with(Props) {
@@ -43,6 +53,10 @@ export default class TheModal extends Vue.with(Props) {
 
   public created () {
     this.editingItems = getInitializedEditingItems(this.items)
+  }
+
+  private checkItem ({ shouldBeChecked, targetItem }: { shouldBeChecked: boolean; targetItem: ItemCheckable }) {
+    this.editingItems = getCheckedItems(shouldBeChecked, targetItem)(this.editingItems)
   }
 }
 </script>
