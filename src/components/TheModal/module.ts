4a6c041f14515fbc3a98aba8a92c4d73f13d046e
minericefield <minericefield@gmail.com>
2021-01-04 21:03:23 +0900

    add checking to TheModal module

--- src/components/TheModal/module.ts
+++ src/components/TheModal/module.ts
@@ -1,7 +1,14 @@
 import { Item, ItemCheckable } from '@/types'
 
-import { map } from 'ramda'
+import { equals, map, when } from 'ramda'
 import { addIsChecked } from '@/utilities'
+
+export const getCheckedItems = (shouldBeChecked: boolean, targetItem: ItemCheckable) => map(
+  when(
+    equals(targetItem),
+    addIsChecked(shouldBeChecked)
+  )
+)
 
 export const getInitializedEditingItems = map<Item, ItemCheckable>(
   addIsChecked(false)
