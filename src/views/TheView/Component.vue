65c632994257dbc560d656fc7f132df3a9ec7c4f
minericefield <minericefield@gmail.com>
2021-01-04 20:45:36 +0900

    introducing TheModal to TheView

--- src/views/TheView/Component.vue
+++ src/views/TheView/Component.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="the-view">
+    <nav class="navbar navbar-light bg-light">
+      <button
+        @click="isModalVisible = true"
+        type="button"
+        class="btn btn-primary ml-auto"
+      >
+        Edit
+      </button>
+    </nav>
     <ul class="list-group">
       <li
         v-for="item in items"
@@ -9,6 +18,13 @@
         {{ item.name }}
       </li>
     </ul>
+    <teleport to="#modal-overlay">
+      <the-modal
+        v-if="isModalVisible"
+        :items="items"
+        @on-cancel-click="isModalVisible = false"
+      />
+    </teleport>
     <teleport to="#loader-overlay">
       <the-loader v-if="isLoaderVisible" />
     </teleport>
@@ -21,15 +37,18 @@ import { Item } from '@/types'
 import { Options, Vue } from 'vue-class-component'
 import { fetchItems } from './module'
 
+import TheModal from '@/components/TheModal/Component.vue'
 import TheLoader from '@/components/TheLoader/Component.vue'
 
 @Options({
   components: {
+    TheModal,
     TheLoader
   }
 })
 export default class TheView extends Vue {
   private items: Item[] = []
+  private isModalVisible = false
   private isLoaderVisible = false
 
   public async created () {
